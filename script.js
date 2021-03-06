f16055bbd006d6bc8bc62bc0aad53471b4b2207d
Neil Creveling <neil.creveling@gmail.com>
2021-03-06 15:30:24 -0500

    built colorCoding function to color timeblocks based on time

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 // get current date and display it in header //
 var currentDate = moment().format("dddd, MMMM Do, YYYY");
 $("#currentDay").text(currentDate);
+
+// build colorCoding function to color text areas based on time //
+function colorCoding() {
+    // loop through each time block using each() method
+    $(".time-block").each(function(hour, item) {
+        // create variable for the current hour (using moment)
+        var currentHour = moment().format("HH");
+        // use parseInt function to take the string of current hours and convert them into integeers
+        var currentInt = parseInt(currentHour);
+        // use parseInt function to take the string of data-hour elements and conver them into integers
+        var blockValue = parseInt($(item).data("hour"));
+        // now one can compare the current hour to the data-time value attributed to the timeblocks since they are both integers
+        if (blockValue === currentInt) {
+            $(item).addClass("present")
+        } else if (blockValue < currentInt) {
+            $(item).addClass("past")
+        } else {
+            $(item).addClass("future")
+        }
+    });
+}
+
+
